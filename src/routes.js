13943c396ef8f7052ba9289ccb9fa6bc64d5e526
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2019-09-05 20:49:58 -0300

    Rota de Arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
+import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
